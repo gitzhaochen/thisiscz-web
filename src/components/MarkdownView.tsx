74c075dcb7132c970816d23e6eddb5c33f96f54b
cz <allenzhaochen@gmail.com>
2026-09-10 12:08:02 +1200

    feat: update

--- src/components/MarkdownView.tsx
+++ src/components/MarkdownView.tsx
@@ -4,7 +4,7 @@ import rehypeHighlight from 'rehype-highlight'
 import 'highlight.js/styles/github-dark.css'
 const MarkdownView = ({ content }: { content: string }) => {
   return (
-    <div className="prose prose-zinc dark:prose-invert text-foreground max-w-none">
+    <div className="prose prose-zinc dark:prose-invert prose-img:mx-auto prose-img:block prose-img:max-w-full md:prose-img:max-w-[70%] text-foreground max-w-none">
       <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>
         {content}
       </Markdown>
